2e5706abd916d0f3c99ca4da684edb923171501d
David Samuelson <dsamuelson89@gmail.com>
2022-04-05 13:58:06 -0600

    made db query call

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'Int3rn3t!',
+        database:'election'
+    },
+    console.log('connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
   app.use((req, res) => {
     res.status(404).end();
   });
